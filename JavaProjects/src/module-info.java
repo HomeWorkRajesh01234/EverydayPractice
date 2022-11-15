3eec57b068ca321d48c05a774180582f5bd8c508
prpho <prpho@Pratik>
2022-11-15 22:02:22 +0530

    1st

---
+++ JavaProjects/src/module-info.java
@@ -0,0 +1,20 @@
+/**1)Linked List / Lists / ArrayLists differences
+ * 2) Why iterator is used in Stack
+ * 3)
+ * 4)
+ * 5)
+ * 6)
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ * 
+ */
+/**
+ * @author prpho
+ *
+ */
+module JavaProj {
+}
