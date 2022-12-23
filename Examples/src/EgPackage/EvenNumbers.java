caf8735801a44d1cac702f3158ba7d251470dc3f
prpho <prpho@activate.adobe.com>
2022-12-24 01:02:30 +0530

    Done

--- Examples/src/EgPackage/EvenNumbers.java
+++ Examples/src/EgPackage/EvenNumbers.java
@@ -22,6 +22,6 @@ public class EvenNumbers {
 			{
 				System.out.println("Even Numbers  "+i);
 			}
-		
+		sc.close();
 	}
 }
--- JavaProjects/src/Package/ZTemporary.java
+++ JavaProjects/src/Package/ZTemporary.java
@@ -1,17 +1,10 @@
 package Package;
-import java.util.Scanner;
+
 public class ZTemporary {
 
 	public static void main(String[] args) {
 		// TODO Auto-generated method stub
-        String str= "there are no traffic rules followed by people";
-        String words[]= str.split("\\s");
-        String capitalizeword = "";
-        for (String W:words) 
-        {
-        	String first = W.substring(0,1);
-        	String afterfirst = W.substring(1);
-        	capitalizeword+=first.toUpperCase()+afterfirst+""; 
-        }
+        
+        
 	}
 }
